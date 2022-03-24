049b32cf1e674569d20a52f685a095b608ddce2d
Faustinien <kfaustinien@gmail.com>
2022-03-24 16:57:40 +0100

    add action update_user + add action softDeleteCommentary + double call route soft_delete_commentary

--- src/Controller/CommentaryController.php
+++ src/Controller/CommentaryController.php
@@ -60,4 +60,62 @@ class CommentaryController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+
+
+
+    //  1ère FACON :
+    //              Inconvénient : C'est très verbeux.  
+    //                             Les paramètres attendus de la route pour faire un redirectoRoute() peuvent ne pas être accessibles.
+    //              Avantage:~~~~~~~la redirection sera STATIQUE, tous les utilisateurs seront redirigés au même endroits.
+
+    
+
+    //   2ème FAÇON => Inconvénient :  La redirection se fera en fonction de l'url de provenance de la requête, à savoir si vous utilisez cette action à plusieurs endroits différents de votre site, l'utilisateur sera redirigé ailleurs que ce que vous avez décidé.
+    //                 Avantage:~~~~~~~~ la redirection devient DYNAMIQUE. (elle changera en fonctionde la provenance de la requête)
+
+    /** 
+     * @Route("/archiver-mon-commentaire_{id}", name="soft_delete_commentary", methods={"GET"})
+     */
+    public function softDeleteCommentary(Commentary $commentary, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        // * PARCE QUE nous allons rediriger vers 'show_article' qui attend 3 arguments, nous avons injecté l'objet Request ↑↑↑
+
+        #================ 1ere facon ==================#
+        //     dd($request->query->get('article_alias'));
+        #==============================================#
+
+
+        #================= 2eme facon ==================#
+        //     dd($request->server->get('HTTP_REFERER'));
+        #===============================================#
+
+        $commentary->setDeletedAt(new DateTime());
+
+        $entityManager->persist($commentary);
+        $entityManager->flush();
+
+        $this->addFlash('success', "votre commentaires est archivée");
+        #====================== 1ere facon ==================#
+        # La construction de l'URL a lieu dans le fichier 'show_article.html.twig' sur l'attibut HTML 'href' de la balise <a>.
+        # ==> VOIR 'show_article.html.twig' pour la suite de la 1ere FAÇON
+
+
+        # Ici nous récupérons les valeurs des paramètres passés dans l'url $_GET (query)
+        //        return $this->redirectToRoute('show_article', [
+        //            'cat_alias' => $request->query->get('cat_alias'),
+        //            'article_alias' => $request->query->get('article_alias'),
+        //            'id' => $request->query->get('article_id')
+        //        ]);
+        #====================================================================#
+
+        #========================2eme façon =================================#
+        #Pour cette facon nous avons retirés les paramètres de l'URL dans le fichier'show_article.html.twig'
+        #===> Voir 'show_article.html.twig' pour la suite de la 2éme FACON
+
+        #Ici nous utilisons une clé du $_SERVER (server) qui s'appelle 'HTTP_REFERER'.
+        # ette clé contient l'URL de provenance de la requête ($request).
+        return $this->redirect($request->server->get('HTTP_REFERER'));
+        #====================================================================#
+    }
 }
--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -13,26 +14,54 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterFormType extends AbstractType
 {
+
+    # Nous avons déclaré une propiété de class, car la fonction buildForm() ne peut prendre aucune injection de dépendance.
+    private $security;
+
+
+    public function __construct(Security $security)
+    {
+
+        $this->security = $security;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Votre Email'
-            ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Choisissez un mot de passe'
-            ])
+            ]);
+
+
+
+        # Si c'est un update_user, alors on ne rend pas l'input du password.
+        # Ce champ est donc réservé à l'inscription.
+        if (null === $this->security->getUser()) {
+            $builder
+                ->add('password', PasswordType::class, [
+                    'label' => 'Choisissez un mot de passe'
+                ]);
+        }
+
+
+
+
+
+        $builder
             ->add('prenom', TextType::class, [
                 'label' => 'Votre prenom'
             ])
             ->add('nom', TextType::class, [
                 'label' => 'Votre nom'
             ])
+
+
             ->add('submit', SubmitType::class, [
-                'label' => "Je m'inscris",
+                'label' => null === $this->security->getUser()  ? "Je m'inscris" : "j'actualise mon compte",
                 'validate' => false,
                 'attr' => [
-                    'class' => 'd-block col-2 my-3 mx-auto btn btn-warning'
+                    'class' => 'd-block col-5 my-3 mx-auto btn btn-warning'
 
                 ]
             ]);
